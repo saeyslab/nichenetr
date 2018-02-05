928e5297dfea2e1b71723484570debaf71dd9ea0
browaeysrobin <robin.browaeys@ugent.be>
2018-02-05 15:11:19 +0100

    Test making packages

--- R/hello.R
+++ R/hello.R
@@ -13,9 +13,33 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 #   Reload all code:              'Ctrl + Shift + L'!!!! #   Reload all code:              'Ctrl + Shift + L'!!!!
+#   Reload all code:              'Ctrl + Shift + D'!!!! #   Document all code  devtools::document()
 
+# devtools::use_package("tidyverse")
 
-hello <- function() {
-  print("Hello, world!")
+#' Say hello to whoever you want!
+#'
+#' return hello to everyone, everything you want
+#'
+#' blababjlballdfdfjdlfjdfkdfjdmfkldjfkdqjfmsldqjfmkldqfjsdqmlfjsdqmlfjsdqmfjqmfjsdqmfjqmj
+#' dfdfdfdfddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddfdfdf
+#'
+#' @param a Character vector. The name of whoever you want to greet. must follow following format
+#' \describe{
+#'  \item{grn}{Gene regulatory network}
+#'  \item{sig}{signaling network}
+#'  }
+#'
+#' @return a string
+#' @examples
+#' hello("Hadley")
+#' hello("World")
+#' @family test functions
+hello <- function(a = "world") {
+  # print(paste0(c("Hello,",a)))
+  paste0(c("Hello,",a))
 }
-ddd
+# hello("Hadley")
+
+
+
