6a05856312737637fbb4d95e1bf07b8a4c8270fb
Robin Browaeys <robin.browaeys@ugent.be>
2020-05-14 11:25:57 +0200

    Let users pick assays themselves for get_expressed_genes function

--- R/application_prediction.R
+++ R/application_prediction.R
@@ -1038,11 +1038,12 @@ nichenet_seuratobj_aggregate = function(receiver, seurat_obj, condition_colname,
 #'
 #' @description \code{get_expressed_genes} Return the genes that are expressed in a given cell cluster based on the fraction of cells in that cluster that should express the cell.
 #' @usage
-#' get_expressed_genes(ident, seurat_obj, pct = 0.10)
+#' get_expressed_genes(ident, seurat_obj, pct = 0.10, assay_oi = NULL)
 #'
 #' @param ident Name of cluster identity/identities of cells
 #' @param seurat_obj Single-cell expression dataset as Seurat v3 object https://satijalab.org/seurat/. Should contain a column "aggregate" in the metadata. This column indicates the condition/sample where cells came from.
 #' @param pct We consider genes expressed if they are expressed in at least a specific fraction of cells of a cluster. This number indicates this fraction. Default: 0.10. Choice of this parameter is important and depends largely on the used sequencing platform. We recommend to require a lower fraction (like the default 0.10) for 10X data than for e.g. Smart-seq2 data.
+#' @param assay_oi If wanted: specify yourself which assay to look for. Default this value is NULL and as a consequence the 'most advanced' assay will be used to define expressed genes.
 #'
 #' @return A character vector with the gene symbols of the expressed genes
 #'
@@ -1053,96 +1054,119 @@ nichenet_seuratobj_aggregate = function(receiver, seurat_obj, condition_colname,
 #'
 #' @export
 #'
-get_expressed_genes = function(ident, seurat_obj, pct = 0.1){
+get_expressed_genes = function(ident, seurat_obj, pct = 0.1, assay_oi = NULL){
   library(Seurat)
   library(dplyr)
 
-  if(! "RNA" %in% names(seurat_obj@assays)){
-    if ("Spatial" %in% names(seurat_obj@assays)){
-      if (class(seurat_obj@assays$Spatial@data) != "matrix" & class(seurat_obj@assays$Spatial@data) != "dgCMatrix") {
+  # input check
+
+
+  if (!"RNA" %in% names(seurat_obj@assays)) {
+    if ("Spatial" %in% names(seurat_obj@assays)) {
+      if (class(seurat_obj@assays$Spatial@data) != "matrix" &
+          class(seurat_obj@assays$Spatial@data) != "dgCMatrix") {
         warning("Spatial Seurat object should contain a matrix of normalized expression data. Check 'seurat_obj@assays$Spatial@data' for default or 'seurat_obj@assays$SCT@data' for when the single-cell transform pipeline was applied")
       }
       if (sum(dim(seurat_obj@assays$Spatial@data)) == 0) {
         stop("Seurat object should contain normalized expression data (numeric matrix). Check 'seurat_obj@assays$Spatial@data'")
       }
-    }} else {
-      if (class(seurat_obj@assays$RNA@data) != "matrix" &
-          class(seurat_obj@assays$RNA@data) != "dgCMatrix") {
-        warning("Seurat object should contain a matrix of normalized expression data. Check 'seurat_obj@assays$RNA@data' for default or 'seurat_obj@assays$integrated@data' for integrated data or seurat_obj@assays$SCT@data for when the single-cell transform pipeline was applied")
-      }
-
-      if ("integrated" %in% names(seurat_obj@assays)) {
-        if (sum(dim(seurat_obj@assays$RNA@data)) == 0 & sum(dim(seurat_obj@assays$integrated@data)) ==
-            0)
-          stop("Seurat object should contain normalized expression data (numeric matrix). Check 'seurat_obj@assays$RNA@data' for default or 'seurat_obj@assays$integrated@data' for integrated data")
-      }
-      else if ("SCT" %in% names(seurat_obj@assays)) {
-        if (sum(dim(seurat_obj@assays$RNA@data)) == 0 & sum(dim(seurat_obj@assays$SCT@data)) ==
-            0) {
-          stop("Seurat object should contain normalized expression data (numeric matrix). Check 'seurat_obj@assays$RNA@data' for default or 'seurat_obj@assays$SCT@data' for data corrected via SCT")
-        }
-      }
-      else {
-        if (sum(dim(seurat_obj@assays$RNA@data)) == 0) {
-          stop("Seurat object should contain normalized expression data (numeric matrix). Check 'seurat_obj@assays$RNA@data'")
-        }
+    }
+  }
+  else {
+    if (class(seurat_obj@assays$RNA@data) != "matrix" &
+        class(seurat_obj@assays$RNA@data) != "dgCMatrix") {
+      warning("Seurat object should contain a matrix of normalized expression data. Check 'seurat_obj@assays$RNA@data' for default or 'seurat_obj@assays$integrated@data' for integrated data or seurat_obj@assays$SCT@data for when the single-cell transform pipeline was applied")
+    }
+    if ("integrated" %in% names(seurat_obj@assays)) {
+      if (sum(dim(seurat_obj@assays$RNA@data)) == 0 & sum(dim(seurat_obj@assays$integrated@data)) ==
+          0)
+        stop("Seurat object should contain normalized expression data (numeric matrix). Check 'seurat_obj@assays$RNA@data' for default or 'seurat_obj@assays$integrated@data' for integrated data")
+    }
+    else if ("SCT" %in% names(seurat_obj@assays)) {
+      if (sum(dim(seurat_obj@assays$RNA@data)) == 0 & sum(dim(seurat_obj@assays$SCT@data)) ==
+          0) {
+        stop("Seurat object should contain normalized expression data (numeric matrix). Check 'seurat_obj@assays$RNA@data' for default or 'seurat_obj@assays$SCT@data' for data corrected via SCT")
       }
     }
-
+    else {
+      if (sum(dim(seurat_obj@assays$RNA@data)) == 0) {
+        stop("Seurat object should contain normalized expression data (numeric matrix). Check 'seurat_obj@assays$RNA@data'")
+      }
+    }
+  }
   if (sum(ident %in% unique(Idents(seurat_obj))) != length(ident)) {
     stop("One or more provided cell clusters is not part of the 'Idents' of your Seurat object")
   }
 
-  cells_oi = Idents(seurat_obj) %>% .[Idents(seurat_obj) %in% ident] %>% names()
-  if ("integrated" %in% names(seurat_obj@assays)) {
-    warning("Seurat object is result from the Seurat integration workflow. Make sure that this way of defining expressed genes is appropriate for your integrated data. The expressed genes are defined based on the integrated slot.")
-    cells_oi_in_matrix = intersect(colnames(seurat_obj@assays$integrated@data),
-                                   cells_oi)
-    if (length(cells_oi_in_matrix) != length(cells_oi))
-      stop("Not all cells of interest are in your expression matrix (seurat_obj@assays$integrated@data). Please check that the expression matrix contains cells in columns and genes in rows.")
-    exprs_mat = seurat_obj@assays$integrated@data %>% .[,
-                                                        cells_oi_in_matrix]
-  }
-  else if ("SCT" %in% names(seurat_obj@assays) & !"Spatial" %in% names(seurat_obj@assays)) {
-    warning("Seurat object is result from the Seurat single-cell transform workflow. Make sure that this way of defining expressed genes is appropriate for SCT data. The expressed genes are defined based on the SCT slot.")
-    cells_oi_in_matrix = intersect(colnames(seurat_obj@assays$SCT@data),
-                                   cells_oi)
-    if (length(cells_oi_in_matrix) != length(cells_oi))
-      stop("Not all cells of interest are in your expression matrix (seurat_obj@assays$SCT@data). Please check that the expression matrix contains cells in columns and genes in rows.")
-    exprs_mat = seurat_obj@assays$SCT@data %>% .[, cells_oi_in_matrix]
-  }
-  else if ("Spatial" %in% names(seurat_obj@assays) & !"SCT" %in% names(seurat_obj@assays)) {
-    warning("Seurat object is result from the Seurat spatial object. Make sure that this way of defining expressed genes is appropriate for Spatial data.")
-    cells_oi_in_matrix = intersect(colnames(seurat_obj@assays$Spatial@data),
-                                   cells_oi)
-    if (length(cells_oi_in_matrix) != length(cells_oi))
-      stop("Not all cells of interest are in your expression matrix (seurat_obj@assays$Spatial@data). Please check that the expression matrix contains cells in columns and genes in rows.")
-    exprs_mat = seurat_obj@assays$Spatial@data %>% .[, cells_oi_in_matrix]
+  if(!is.null(assay_oi)){
+    if(! assay_oi %in% Assays(seurat_obj)){
+      stop("assay_oi should be an assay of your Seurat object")
+    }
   }
 
-  else if ("Spatial" %in% names(seurat_obj@assays) & "SCT" %in% names(seurat_obj@assays)) {
-    warning("Seurat object is result from the Seurat spatial object, followed by the SCT workflow. Make sure that this way of defining expressed genes is appropriate for Spatial data. The expressed genes are defined based on the SCT slot.")
-    # cells_oi_in_matrix = intersect(colnames(seurat_obj@assays$Spatial@data),
-    #                                cells_oi)
-    cells_oi_in_matrix = intersect(colnames(seurat_obj@assays$SCT@data),
-                                   cells_oi)
-    if (length(cells_oi_in_matrix) != length(cells_oi))
-      stop("Not all cells of interest are in your expression matrix (seurat_obj@assays$Spatial@data). Please check that the expression matrix contains cells in columns and genes in rows.")
-    # exprs_mat = seurat_obj@assays$Spatial@data %>% .[, cells_oi_in_matrix] # which one?
-    exprs_mat = seurat_obj@assays$SCT@data %>% .[, cells_oi_in_matrix] # which one?
+  # Get cell identities of cluster of interest
+
+
+  cells_oi = Idents(seurat_obj) %>% .[Idents(seurat_obj) %in%
+                                        ident] %>% names()
+
+  # Get exprs matrix: from assay oi or from most advanced assay if assay oi not specifcied
+
+  if(!is.null(assay_oi)){
+    cells_oi_in_matrix = intersect(colnames(seurat_obj[[assay_oi]]@data), cells_oi)
+    exprs_mat = seurat_obj[[assay_oi]]@data %>% .[, cells_oi_in_matrix]
+  } else {
+    if ("integrated" %in% names(seurat_obj@assays)) {
+      warning("Seurat object is result from the Seurat integration workflow. Make sure that this way of defining expressed genes is appropriate for your integrated data. The expressed genes are defined based on the integrated slot.")
+      cells_oi_in_matrix = intersect(colnames(seurat_obj@assays$integrated@data),
+                                     cells_oi)
+      if (length(cells_oi_in_matrix) != length(cells_oi))
+        stop("Not all cells of interest are in your expression matrix (seurat_obj@assays$integrated@data). Please check that the expression matrix contains cells in columns and genes in rows.")
+      exprs_mat = seurat_obj@assays$integrated@data %>% .[,
+                                                          cells_oi_in_matrix]
+    }
+    else if ("SCT" %in% names(seurat_obj@assays) & !"Spatial" %in%
+             names(seurat_obj@assays)) {
+      warning("Seurat object is result from the Seurat single-cell transform workflow. Make sure that this way of defining expressed genes is appropriate for SCT data. The expressed genes are defined based on the SCT slot.")
+      cells_oi_in_matrix = intersect(colnames(seurat_obj@assays$SCT@data),
+                                     cells_oi)
+      if (length(cells_oi_in_matrix) != length(cells_oi))
+        stop("Not all cells of interest are in your expression matrix (seurat_obj@assays$SCT@data). Please check that the expression matrix contains cells in columns and genes in rows.")
+      exprs_mat = seurat_obj@assays$SCT@data %>% .[, cells_oi_in_matrix]
+    }
+    else if ("Spatial" %in% names(seurat_obj@assays) &
+             !"SCT" %in% names(seurat_obj@assays)) {
+      warning("Seurat object is result from the Seurat spatial object. Make sure that this way of defining expressed genes is appropriate for Spatial data.")
+      cells_oi_in_matrix = intersect(colnames(seurat_obj@assays$Spatial@data),
+                                     cells_oi)
+      if (length(cells_oi_in_matrix) != length(cells_oi))
+        stop("Not all cells of interest are in your expression matrix (seurat_obj@assays$Spatial@data). Please check that the expression matrix contains cells in columns and genes in rows.")
+      exprs_mat = seurat_obj@assays$Spatial@data %>% .[, cells_oi_in_matrix]
+    }
+    else if ("Spatial" %in% names(seurat_obj@assays) &
+             "SCT" %in% names(seurat_obj@assays)) {
+      warning("Seurat object is result from the Seurat spatial object, followed by the SCT workflow. Make sure that this way of defining expressed genes is appropriate for Spatial data. The expressed genes are defined based on the SCT slot.")
+      cells_oi_in_matrix = intersect(colnames(seurat_obj@assays$SCT@data),
+                                     cells_oi)
+      if (length(cells_oi_in_matrix) != length(cells_oi))
+        stop("Not all cells of interest are in your expression matrix (seurat_obj@assays$Spatial@data). Please check that the expression matrix contains cells in columns and genes in rows.")
+      exprs_mat = seurat_obj@assays$SCT@data %>% .[, cells_oi_in_matrix]
+    }
+    else {
+      if (sum(cells_oi %in% colnames(seurat_obj@assays$RNA@data)) ==
+          0)
+        stop("None of the cells are in colnames of 'seurat_obj@assays$RNA@data'. The expression matrix should contain cells in columns and genes in rows.")
+      cells_oi_in_matrix = intersect(colnames(seurat_obj@assays$RNA@data),
+                                     cells_oi)
+      if (length(cells_oi_in_matrix) != length(cells_oi))
+        stop("Not all cells of interest are in your expression matrix (seurat_obj@assays$RNA@data). Please check that the expression matrix contains cells in columns and genes in rows.")
+      exprs_mat = seurat_obj@assays$RNA@data %>% .[, cells_oi_in_matrix]
+    }
 
   }
 
-  else {
-    if (sum(cells_oi %in% colnames(seurat_obj@assays$RNA@data)) ==
-        0)
-      stop("None of the cells are in colnames of 'seurat_obj@assays$RNA@data'. The expression matrix should contain cells in columns and genes in rows.")
-    cells_oi_in_matrix = intersect(colnames(seurat_obj@assays$RNA@data),
-                                   cells_oi)
-    if (length(cells_oi_in_matrix) != length(cells_oi))
-      stop("Not all cells of interest are in your expression matrix (seurat_obj@assays$RNA@data). Please check that the expression matrix contains cells in columns and genes in rows.")
-    exprs_mat = seurat_obj@assays$RNA@data %>% .[, cells_oi_in_matrix]
-  }
+  # use defined cells and exprs matrix to get expressed genes
+
   n_cells_oi_in_matrix = length(cells_oi_in_matrix)
   if (n_cells_oi_in_matrix < 5000) {
     genes = exprs_mat %>% apply(1, function(x) {
