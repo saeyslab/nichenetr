2ee4365c882daac13fcfe66e24c485566d2f0b3f
Robin Browaeys <robin.browaeys@ugent.be>
2020-07-06 10:38:23 +0200

    change test to refine bug make_discrete_ligand_target_matrix()

--- tests/testthat/test-application_networks.R
+++ tests/testthat/test-application_networks.R
@@ -28,6 +28,25 @@ test_that("active ligand-target matrix and network can be constructed", {
   setting = lapply(expression_settings_validation[1:3],convert_expression_settings_evaluation)
   ligands = extract_ligands_from_settings(setting)
   ligand_target_matrix = construct_ligand_target_matrix(weighted_networks, ligands)
+
+  # temporary debugging solution:
+  ligands = colnames(ligand_target_matrix)
+  targets = rownames(ligand_target_matrix)
+  error_rate = 0.1
+  cutoff_method = "quantile"
+  fdr_method = "global"
+  ligands_position = "cols"
+  list_targets = lapply(ligands,get_target_genes_ligand_oi,ligand_target_matrix,cutoff_method = cutoff_method, fdr_method = fdr_method,error_rate = error_rate)
+  print(length(list_targets))
+  print(length(list_targets[[1]]))
+  names(list_targets) = ligands
+  ligand_target_matrix_discrete = list_targets %>% bind_rows() %>% as.matrix()
+  print(dim(ligand_target_matrix_discrete))
+  expect_equal(dim(ligand_target_matrix_discrete), dim(ligand_target_matrix))
+
+  rownames(ligand_target_matrix_discrete) = targets %>% make.names() # maybe change this again
+  expect_equal(dim(ligand_target_matrix_discrete), dim(ligand_target_matrix))
+
   ligand_target_matrix_discrete = make_discrete_ligand_target_matrix(ligand_target_matrix, cutoff_method = "quantile")
   active_lt = get_active_ligand_target_matrix(setting[[1]] %>% .$response, ligand_target_matrix_discrete)
   active_lt_df = get_active_ligand_target_df(setting[[1]] %>% .$response, ligand_target_matrix_discrete)
