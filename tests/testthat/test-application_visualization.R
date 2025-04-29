efbd649024ab1cf2110c21fa76292d73402b335a
Victor Medaerts <victor.medaerts@skynet.be>
2025-04-29 09:25:12 +0200

    changed to lr_network_human in test-application_vizualization.R

--- tests/testthat/test-application_visualization.R
+++ tests/testthat/test-application_visualization.R
@@ -10,7 +10,7 @@ test_that("ligand-target signaling paths can be visualized", {
   k = 2
 
   ligand_target_signaling_list = get_ligand_signaling_path(ligand_tf_matrix,all_ligands,all_targets,k,weighted_networks)
-  data_source_info_network = infer_supporting_datasources(ligand_target_signaling_list, lr_network, sig_network, gr_network)
+  data_source_info_network = infer_supporting_datasources(ligand_target_signaling_list, lr_network_human, sig_network, gr_network)
   graph = diagrammer_format_signaling_graph(ligand_target_signaling_list, all_ligands,all_targets)
 
   expect_type(ligand_target_signaling_list,"list")
